923a5807270c9ac328f6f9350ea3d59d054c8485
Josué Fernandes <josue7mf@hotmail.com>
2023-11-11 23:38:53 -0300

    :sparkles: feat: Adicionando xpaths as variaveis de ambiente

--- src/core/settings.py
+++ src/core/settings.py
@@ -5,5 +5,10 @@ load_dotenv()
 
 class Settings:
     API_BASE_URL = os.environ.get("API_BASE_URL")
+    XPATH_VALOR_ATUAL = "/html/body/main/div[2]/div/div[1]/div/div[1]/div/div[1]/strong"
+    XPATH_RENDIMENTO_MENSAL = "/html/body/main/div[2]/div/div[1]/div/div[5]/div/div[1]/strong"
+    XPATH_ALTERACAO_ATUAL = "/html/body/main/div[2]/div/div[1]/div/div[1]/div/div[2]/span/b"
+    XPATH_TABLE = "/html/body/main/div[3]/div/div[1]/div[2]/div[7]/div/div[2]/table"
+
 
 settings = Settings()
